4ca351612efe4a689e49068ad0417efd466c3ab8
Administrator <2912240796@qq.com>
2020-07-04 11:41:38 +0800

    20200704

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import router from "./router"; // 路由
 import store from "./store"; // vuex
-import EVueContextmenu from 'e-vue-contextmenu'; // 右键菜单
+import EVueContextmenu from "e-vue-contextmenu"; // 右键菜单
 import "./styles/init.css"; // 样式初始化
 import "./styles/global.css"; // 全局样式
-import './utils/EUI'; // element-ui
-import './utils/directive'; // 指令
-import './utils/filter'; // 过滤器
-import './icons'; // svg
+import "./utils/EUI"; // element-ui
+import "./utils/directive"; // 指令
+import "./utils/filter"; // 过滤器
+import "./icons"; // svg
 import App from "./App.vue";
 
 Vue.use(EVueContextmenu);
